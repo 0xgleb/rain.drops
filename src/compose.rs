60c30d6c5bd99ffcd306388750a8b5da414232c3
0xgleb <gleb.dianov@protonmail.com>
2025-03-26 23:23:24 +0800

    generate arg triplet

--- src/compose.rs
+++ src/compose.rs
@@ -96,70 +96,136 @@ mod tests {
     proptest! {
         #[test]
         fn test_enrich_and_merge(
-            clearv2_logs in arb_trade_logs(TradeEvent::ClearV2),
-            takeorderv2_logs in arb_trade_logs(TradeEvent::TakeOrderV2),
+            (clearv2_trades, takeorderv2_trades, block_bodies) in arb_enrich_and_merge_args()
         ) {
-            let block_numbers = clearv2_logs
-                .iter()
-                .chain(takeorderv2_logs.iter())
-                .map(|log| log.block_number)
-                .sorted()
-                .collect_vec();
+            println!("clearv2_trades: {:?}", clearv2_trades);
+            println!("takeorderv2_trades: {:?}", takeorderv2_trades);
+            println!("block_bodies: {:?}", block_bodies);
+        }
+    }
 
+    fn arb_enrich_and_merge_args() -> impl Strategy<
+        Value = (
+            BTreeMap<BlockNumber, Vec<TradeLog>>,
+            BTreeMap<BlockNumber, Vec<TradeLog>>,
+            BTreeMap<BlockNumber, BlockMetadata>,
+        ),
+    > {
+        arb_trade_logs_and_hashes().prop_flat_map(
+            |(clearv2_trades, takeorderv2_trades, block_num_to_tx_hashes)| {
+                // Create a strategy for the block metadata map
+                let block_numbers: Vec<BlockNumber> =
+                    block_num_to_tx_hashes.keys().copied().collect();
+                let block_metadata_strategy = prop::collection::btree_map(
+                    Just(block_numbers),
+                    arb_block_metadata(
+                        block_num_to_tx_hashes.values().next().unwrap().clone(),
+                    ),
+                    1..=block_num_to_tx_hashes.len(),
+                )
+                .prop_map(|map| {
+                    map.into_iter()
+                        .map(|(k, v)| (k[0], v))
+                        .collect::<BTreeMap<_, _>>()
+                });
+
+                // Combine the trade logs with the block metadata
+                (
+                    Just(clearv2_trades),
+                    Just(takeorderv2_trades),
+                    block_metadata_strategy,
+                )
+                    .prop_map(
+                        |(clearv2, takeorderv2, blocks)| {
+                            (clearv2, takeorderv2, blocks)
+                        },
+                    )
+            },
+        )
+    }
+
+    prop_compose! {
+        fn arb_trade_logs_and_hashes()(
+            clearv2_logs in arb_trade_logs(TradeEvent::ClearV2),
+            takeorderv2_logs in arb_trade_logs(TradeEvent::TakeOrderV2)
+        ) -> (
+            BTreeMap<BlockNumber, Vec<TradeLog>>,
+            BTreeMap<BlockNumber, Vec<TradeLog>>,
+            BTreeMap<BlockNumber, Vec<TxHash>>,
+        ) {
             let mut block_num_to_tx_hashes =
                 BTreeMap::<BlockNumber, Vec<(u64, TxHash)>>::new();
 
-            for log in clearv2_logs.clone().into_iter().chain(takeorderv2_logs.clone()) {
+            for log in
+                clearv2_logs.clone().into_iter().chain(takeorderv2_logs.clone())
+            {
                 block_num_to_tx_hashes
                     .entry(log.block_number)
                     .and_modify(|hashes| hashes.push((log.log_index, log.tx_hash)))
                     .or_insert(vec![(log.log_index, log.tx_hash)]);
             }
 
-            prop_assume!(!block_numbers.is_empty());
+            let clearv2_trades: BTreeMap<BlockNumber, Vec<TradeLog>> = clearv2_logs
+                .into_iter()
+                .map(|log| (log.block_number, vec![log]))
+                .collect();
 
-            let min_block_number = block_numbers.first().unwrap();
-            let max_block_number = block_numbers.last().unwrap();
+            let takeorderv2_trades: BTreeMap<BlockNumber, Vec<TradeLog>> =
+                takeorderv2_logs
+                    .into_iter()
+                    .map(|log| (log.block_number, vec![log]))
+                    .collect();
 
-            println!(
-                "\n\nMin block number: {min_block_number}; Max block number: {max_block_number}"
-            );
+            let block_num_to_tx_hashes = block_num_to_tx_hashes
+                .into_iter()
+                .map(|(block_number, hashes_with_indices)| {
+                    let sorted_hashes = hashes_with_indices
+                        .into_iter()
+                        .sorted_by_key(|(index, _)| *index)
+                        .map(|(_, hash)| hash)
+                        .collect_vec();
+                    (block_number, sorted_hashes)
+                })
+                .collect::<BTreeMap<_, _>>();
 
-            println!(
-                "\n\nBlock num to tx hashes: {block_num_to_tx_hashes:?}"
-            );
+            (clearv2_trades, takeorderv2_trades, block_num_to_tx_hashes)
         }
     }
 
-    // fn arb_blocks_with_trades(
-    //     block_num_to_tx_hashes: BTreeMap<BlockNumber, Vec<(u64, TxHash)>>,
-    // ) -> impl Strategy<Value = BTreeMap<BlockNumber, BlockMetadata>> {
-    //     prop::collection::btree_map(
-    //         prop::sample::Index::new(
-    //             block_num_to_tx_hashes.keys().collect_vec(),
-    //         ),
-    //         arb_block_metadata(block_num_to_tx_hashes.get(&index).unwrap()),
-    //     )
-    // }
+    prop_compose! {
+        fn arb_block_metadata(required_tx_hashes: Vec<TxHash>)(
+            timestamp in 0u64..10000,
+            transactions in arb_transactions(required_tx_hashes)
+        ) -> BlockMetadata {
+            BlockMetadata { timestamp, transactions }
+        }
+    }
 
-    // prop_compose! {
-    //     fn arb_block_metadata(required_txs: Vec<TxMetadata>)(
-    //         random_txs in prop::collection::vec(arb_tx_metadata(), 0..1000),
-    //     ) -> BlockMetadata {
-    //         let mut all_txs = random_txs;
-    //         all_txs.extend(required_txs);
+    fn arb_transactions(
+        required_tx_hashes: Vec<TxHash>,
+    ) -> impl Strategy<Value = Vec<TxMetadata>> {
+        // First generate the required transactions using arb_tx_metadata_from_hash
+        let required_txs = required_tx_hashes
+            .into_iter()
+            .map(arb_tx_metadata_from_hash)
+            .collect::<Vec<_>>();
 
-    //         let timestamp = 0;
-    //         let transactions = all_txs;
+        // Then generate some arbitrary transactions
+        let arbitrary_txs = prop::collection::vec(arb_tx_metadata(), 0..5);
 
-    //         BlockMetadata { timestamp, transactions }
-    //     }
-    // }
+        // Combine them using prop_flat_map
+        (required_txs, arbitrary_txs).prop_flat_map(|(required, arbitrary)| {
+            // Combine the required and arbitrary transactions
+            let mut all_txs = required;
+            all_txs.extend(arbitrary);
+            Just(all_txs)
+        })
+    }
 
     fn arb_trade_logs(
         event: TradeEvent,
     ) -> impl Strategy<Value = Vec<TradeLog>> {
-        prop::collection::vec(arb_trade_log(event), 0..10)
+        prop::collection::vec(arb_trade_log(event), 1..11)
     }
 
     prop_compose! {
@@ -169,6 +235,14 @@ mod tests {
             tx_hash in arb_tx_hash(),
         ) -> TradeLog {
             TradeLog { log_index, block_number, tx_hash, event: event.clone() }
+        }
+    }
+
+    prop_compose! {
+        fn arb_tx_metadata_from_hash(hash: TxHash)(
+            origin in arb_address()
+        ) -> TxMetadata {
+            TxMetadata { hash, origin }
         }
     }
 
